a36e475bd40b04e7cc339b28e2546e4105e73448
Punya Prashun <punya.prasun.cs24@iilmcet.ac.in>
2024-03-28 22:23:56 +0530

    blueIcon Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {BlueIcon} from './index'
 const Navbar = () => {
     return (
-        <></>
+        <nav className="navbar w-full bg-red-600">
+            <BlueIcon/>
+
+        </nav>
     )
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Navbar, Heading, About } from '../components'
+import { Navbar} from '../components'
 const LandingPage = () => {
   return (
     <>
         <Navbar/>
-        
     </>
   )
 }
