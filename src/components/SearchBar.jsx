22b551bfa8d7a1ee043f7aa905a5d6a4f57ffb27
Punya Prashun <punya.prasun.cs24@iilmcet.ac.in>
2024-03-29 00:14:52 +0530

    searchbar height changed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import Cross from '../assets/close.svg'
 const SearchBar = ({setShowSearchBar}) => {
   return (
     <>
-        <div className="search-bar h-[7vh] w-full absolute top-0 left-0 bg-white px-5 flex justify-between items-center">
+        <div className="search-bar h-[7.2vh] w-full absolute top-0 left-0 bg-white px-5 flex justify-between items-center">
             <img src={SearchIcon} alt="" className='h-7 w-7'/>
             <input type="text" placeholder='Type Your Search...' className='w-[80%] h-7 bg-transparent border-none placeholder:text-[#A9B5C9] outline-none text-[1.125rem]' />
             <img onClick={()=>setShowSearchBar(false)} src={Cross} alt="" className='h-7 w-7'/>
