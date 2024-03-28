46905917caefe87466cacf9bf63e31a7376d871a
Punya Prashun <punya.prasun.cs24@iilmcet.ac.in>
2024-03-28 23:56:11 +0530

    searchBar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import WhiteIcon from './WhiteIcon'
 import Logo from '../assets/kreativan5-01.png'
 import searchIcon from '../assets/search-icon.svg'
 import Hamburger from '../assets/hamburger.svg'
+import SearchBar from './SearchBar'
 const Navbar = () => {
+    const [showSearchBar, setShowSearchBar] = useState(false);
+    const searchBarHandler = ()=>{
+        setShowSearchBar(true);
+    }
     return (
         <nav className="navbar h-[12vh] w-full  flex items-center justify-between px-4">
+            {
+                showSearchBar
+                ?
+                <SearchBar setShowSearchBar={setShowSearchBar}/>
+                :
+                <></>
+            }
             <img src={Logo} alt="" className='w-[16.625rem] aspect-[266 / 67]' />
             <div className="right-box flex items-center">
-                <img src={searchIcon} alt="" className='w-5 h-5'/>
+                <img src={searchIcon} onClick={searchBarHandler} alt="" className='w-5 h-5'/>
                 <WhiteIcon/>
                 <img src={Hamburger} alt=""  className='h-10 w-10'/>
             </div>
