9e1082049e8ef051ef2eefc1486a58b74c386cab
Punya Prashun <punya.prasun.cs24@iilmcet.ac.in>
2024-03-30 03:40:21 +0530

    roundness fixed

--- src/components/CollapsingBox.jsx
+++ src/components/CollapsingBox.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import rightArrow from '../assets/right-arrow.svg'
 const CollapsingBox = ({ques, ans}) => {
   return (
-    <div className='collapsingBox w-full bg-white px-6 lg:px-2 lg:py-4 py-2 flex gap-4 items-center rounded-md'>
+    <div className='collapsingBox w-full bg-white px-6 lg:px-2 lg:py-4 py-2 flex gap-4 items-center rounded-sm'>
         <img src={rightArrow} alt="" className='w-7 h-7'/>
         <p className="question text-[1rem]">
           {ques}
