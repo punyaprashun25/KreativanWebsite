7e113651a75ba5686d87ef1f37b5dff2dd2bac8e
Punya Prashun <punya.prasun.cs24@iilmcet.ac.in>
2024-03-30 02:53:36 +0530

    custom breakpoint changed and created Faq section

--- src/components/CollapsingBox.jsx
+++ src/components/CollapsingBox.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import rightArrow from '../assets/right-arrow.svg'
 const CollapsingBox = ({ques, ans}) => {
   return (
-    <div className='collapsingBox w-full bg-white px-8 py-6 flex gap-4 items-center rounded-lg'>
+    <div className='collapsingBox w-full bg-white px-8 lg:px-2 lg:py-2 py-6 flex gap-4 items-center rounded-md'>
         <img src={rightArrow} alt="" className='w-7 h-7'/>
-        <p className="question text-[1.25rem]">
+        <p className="question text-[1rem]">
         How long do digital marketers have to work?
         </p>
     </div>
