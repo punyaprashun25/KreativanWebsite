f255067e84b75ac0e55fb98422038d576f06d22a
Punya Prashun <punya.prasun.cs24@iilmcet.ac.in>
2024-03-30 03:09:33 +0530

    faq responsiveness fixed

--- src/components/CollapsingBox.jsx
+++ src/components/CollapsingBox.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import rightArrow from '../assets/right-arrow.svg'
 const CollapsingBox = ({ques, ans}) => {
   return (
-    <div className='collapsingBox w-full bg-white px-6 lg:px-2 lg:py-2 py-3 flex gap-4 items-center rounded-md'>
+    <div className='collapsingBox w-full bg-white px-6 lg:px-2 lg:py-4 py-3 flex gap-4 items-center rounded-md'>
         <img src={rightArrow} alt="" className='w-7 h-7'/>
         <p className="question text-[1rem]">
           {ques}
