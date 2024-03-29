53183a17857e9874d359da17f077973b4a64deee
Punya Prashun <punya.prasun.cs24@iilmcet.ac.in>
2024-03-29 22:34:46 +0530

    padding fix

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Heading = () => {
   return (
-    <div className='heading-box w-full px-4 lg:px-12 py-8 mt-16'>
+    <div className='heading-box w-full px-6 lg:px-12 py-8 mt-16'>
       <h2 className="heading-text font-bold text-3xl text-center text-[#1f2531] font-['Inter']">
         A Digital Marketing Company in Chandigarh with a Difference and Proven Expertise.
       </h2>
